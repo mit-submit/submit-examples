3d7bd7472e4564bc91880ca9cefc57cd78b99d67
Benedikt Maier <benedikt.maier@cern.ch>
2021-08-13 18:02:50 -0400

    puma

--- condor_gpu/puma/singularity_puma.sh
+++ condor_gpu/puma/singularity_puma.sh
@@ -6,12 +6,11 @@ unset XDG_RUNTIME_DIR
 # Prepare training config and clone PUMA code
 curpath=$PWD
 sed -i "s|XXX|$curpath|g" train.yaml 
-git clone -b resxy https://github.com/pumaphysics/grapple.git
 
 # Getting ready for singularity
 echo "About to enter the singularity container. This is the content of the current folder: "
 pwd
 ls -l
-SINGULARITYENV_CUDA_VISIBLE_DEVICES=0,1,2,3 singularity exec --nv --bind /mnt/hadoop/cms/store/user/bmaier/:/mnt/hadoop/cms/store/user/bmaier/ --bind $PWD:$PWD docker://benediktmaier/puma:v1 /bin/sh $curpath/train_puma.sh 
+SINGULARITYENV_CUDA_VISIBLE_DEVICES=0,1,2,3 singularity exec --nv --bind /mnt/hadoop/cms/store/user/bmaier/:/mnt/hadoop/cms/store/user/bmaier/ --bind $PWD:$PWD docker://benediktmaier/puma:v230 /bin/sh $curpath/train_puma.sh 
 
 echo "Done."
