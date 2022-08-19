d3f40fcc8bb8923893548bd734e17900163f46ab
Zhangqier Wang <wangzqe@submit04.mit.edu>
2022-08-19 14:11:15 -0400

    add gpu submission (condor)

--- gpu/condor_gpu/exec.sh
+++ gpu/condor_gpu/exec.sh
@@ -1,14 +1,12 @@
-cat .job.ad
-
-export CUDA_VISIBLE_DEVICES=0,1,2
-
-python condor_tensorflow.py
-
+echo `hostname`
+whoami
+id
+source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev4cuda/latest/x86_64-centos7-gcc8-opt/setup.sh
+python condor_torch.py
 echo ">>>>>>>>>>\n"
 echo ""
 ls -a
 echo "<<<<<<<<<<\n"
+#sleep 60m
 echo ""
-cat .machine.ad
-sleep 5
 
